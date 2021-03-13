b2ece11500a7b0a3e185ffa8f7a39d8ab744e0f1
Deepraj Bhosale <deeprajbhosale@pop-os.localdomain>
2021-03-13 17:02:54 +0530

    Added Initial Route along with Dummy Nav and Foot

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-function App() {
+import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from './Pages/Home/home';
+import NavBar from './Components/Navbar';
+import Footer from './Components/Footer';
+
+const App = () => {
+
   return (
-    <div>
-     <h1>Hello World!!</h1>
-    </div>
-  );
+    
+      <Router>
+        <NavBar></NavBar>
+        <Switch>
+          <Route path='/' exact render={()=><Home />}>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+    </Router>
+
+    
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/Pages/Home/home.js
@@ -0,0 +1,26 @@
+//react
+import React, { Fragment } from 'react';
+
+
+//Mui
+
+//Custom Components
+
+
+// const useStyles = makeStyles((theme) => ({
+    
+//  }));
+const Home =() => {
+    
+
+    //const classes = useStyles();
+    
+    return(
+        
+        <Fragment>
+            <div>Hello World</div>
+        </Fragment>
+    )    
+};
+
+export default Home;
