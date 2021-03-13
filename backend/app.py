a60d252a307724c956d2611b4427fa41f17fe802
Deborah <deb.mendes.9@gmail.com>
2021-03-13 18:11:02 +0530

    Database setup

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from dotenv import load_dotenv
+load_dotenv()
+from helpers.database import db
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello!'
-
 if __name__ == "__main__":
     app.run(debug=True)
+
+@app.route('/')
+def hello_world():
+    db.users.insert_one({"name":"het"})
+    return "Test"
+
+
