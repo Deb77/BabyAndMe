9aeab5317503ad8747f8cc854c9b986fda71402b
Deborah <deb.mendes.9@gmail.com>
2021-03-14 14:14:11 +0530

    Test

---
+++ client/src/Components/Admin/Card.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    container: {
+        textAlign: 'center',
+        backgroundColor:'#CBD5F0'
+    },
+    title: {
+        
+    },
+});
+
+export default function MediaCard({ title, img, data }) {
+  const classes = useStyles();
+
+    return (
+      <Card className={classes.container}>
+        <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+            {title}
+        </Typography>
+        <Typography variant="h3" component="h2">
+            {data}
+        </Typography>
+        </CardContent>
+      </Card>
+  );
+}
